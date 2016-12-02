cc91f0746a76541ee2ddbb84c764fee0166ac8b6
Nicholas Donnelly <ndonnelly1006@gmail.com>
2016-12-02 15:53:44 +0100

    Added some assets and a background image that stretches to cover.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,12 @@
+// ASSET FILENAMES
+var trump_correct = ['trump-right-1.jpg', 'trump-right-2.jpg', 'trump-right-3.jpg'];
+var trump_wrong = ['trump-wrong-1.jpg', 'trump-wrong-2.jpg', 'trump-wrong-3.jpg'];
+var trump_intro = 'trump-welcome.jpg';
+
 $(document).ready(function(){
-    $('body').fadeIn('fast');
+    $('body').fadeIn('slow');
+
+    $('.begin-button').click(function(){
+      // logic goes here, hide this div, show next one
+    });
 });
