2d657a1b59bf6ec07fe64cff20428f91e44cf028
Nicholas Donnelly <ndonnelly1006@gmail.com>
2016-12-02 16:03:37 +0100

    Added some animation.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,13 @@
 var trump_correct = ['trump-right-1.jpg', 'trump-right-2.jpg', 'trump-right-3.jpg'];
 var trump_wrong = ['trump-wrong-1.jpg', 'trump-wrong-2.jpg', 'trump-wrong-3.jpg'];
 var trump_intro = 'trump-welcome.jpg';
+var rounds_played = 0;
 
 $(document).ready(function(){
-    $('body').fadeIn('slow');
+    $('.wrapper').css({opacity: '1', transform: 'none'}); // fade it in and move it up a bit
 
     $('.begin-button').click(function(){
       // logic goes here, hide this div, show next one
     });
+    // Maybe 3 options: trump said it, somebody else said it, nobody said it.
 });
